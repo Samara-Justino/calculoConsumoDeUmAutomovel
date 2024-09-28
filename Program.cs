95e440ded1adc0e0b660ccecac5b8b9d47f7bd4f
Samara Justino <samarasouzajustino@gmail.com>
2024-09-28 17:31:03 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.Write("Qual a distância percorrida pelo automóvel em km? ");
+double distancia = double.Parse(Console.ReadLine());
+Console.Write("Qual o volume de combustível em litros? ");
+int volume = int.Parse(Console.ReadLine());
+double consumo = distancia/ volume;
+Console.WriteLine("O consumo médio do automóvel é " + consumo.ToString("F2") + " km/l");
+
